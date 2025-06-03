c88bb71a31a67b17427f30cd816f795843053b6a
Krishnika Goel <krishnika.goel2006@gmail.com>
2025-06-03 23:28:41 +0530

    convert() function added

---
+++ script.js
@@ -0,0 +1,19 @@
+function convert(){
+    let f=document.getElementById("from").value;
+    let t=document.getElementById("to").value;
+    let i=parseFloat(document.getElementById("in").value);
+    let res;
+    if(isNaN(i)){
+        res="Please enter a valid number";
+    }else{
+        let temp;
+        if(f==="Celsius") temp=i;
+        else if(f==="Fahrenheit") temp=(i-32)*5/9;
+        else temp=i-273.15;
+
+        if(t==="Celsius") res=temp + " °C";
+        else if(t==="Fahrenheit") res=(temp*9/5)+32 + " °F";
+        else res=temp+273.15 + " °K";
+    }
+    document.getElementById("res").innerText=res;
+}
